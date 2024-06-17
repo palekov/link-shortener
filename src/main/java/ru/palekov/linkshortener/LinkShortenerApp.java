1c25e2fe08f7c495d4a359a164bf15333c550b06
Palekov Alexander <palekov-2011@mail.ru>
2024-06-17 15:38:03 +0300

    added maven and gradle libs for lesson4

--- src/main/java/ru/palekov/linkshortener/LinkShortenerApp.java
+++ src/main/java/ru/palekov/linkshortener/LinkShortenerApp.java
@@ -1,26 +1,15 @@
 package ru.palekov.linkshortener;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.palekov.linkshortener.dto.CreateShortLinkRequest;
-import ru.palekov.linkshortener.model.LinkInfo;
-import ru.palekov.linkshortener.service.LinkInfoService;
-
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import ru.palekov.LoggingConfigurationGradle;
+import ru.palekov.LoggingConfigurationMaven;
 
 @SpringBootApplication
 public class LinkShortenerApp {
     public static void main(String[] args) {
-        LinkInfoService linkInfoService = new LinkInfoService();
-        CreateShortLinkRequest shortLinkRequest = new CreateShortLinkRequest("my link",
-                ZonedDateTime.of(2030, 10, 30, 0, 0, 0, 0,
-                        ZoneId.of("Europe/Minsk")),
-                "my link description", true );
-
-        LinkInfo linkInfo = linkInfoService.createLinkInfo(shortLinkRequest);
-        String shortLink = linkInfo.getShortLink();
-        System.out.println("Short link is: " + shortLink);
-        System.out.println("getByShortLink result is:" + linkInfoService.getByShortLink(shortLink));
-//        SpringApplication.run(LinkShortenerApp.class);
+        LoggingConfigurationMaven.testLog("test logging from maven lib is working!");
+        LoggingConfigurationGradle.testLog("test logging from gradle lib is working!");
+        SpringApplication.run(LinkShortenerApp.class);
     }
 }
